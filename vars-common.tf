d48dcd28ccb51288fe025a4dd8c023f0b13322a4
Patrick Clough <patrick@sibbit.co.uk>
2024-04-04 15:13:44 +0100

    added vars-common.tf with expected use defaults

---
+++ vars-common.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  default = "CSFD_Test"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_key" {
+  default = "CSFD"
+}
+
+variable "application_domain" {
+  default = "default_server"
+}
+
+variable "git_repo" {
+  default = "https://github.com/PCloughster/CSFD-test-repo"
+}
+
+variable "git_repo_name" {
+  default = "CSFD-test-repo"
+}
