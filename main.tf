227f615bfc486da1c1de98d7aa89839b1c302169
Patrick Clough <patrick@sibbit.co.uk>
2024-04-04 15:16:03 +0100

    added security group rules to main.tf for 80 + 443

--- main.tf
+++ main.tf
@@ -1,3 +1,48 @@
+resource "aws_security_group" "vm" {
+  name        = "${var.project_name}-sg"
+  description = "network rules for VM"
+}
+
+resource "aws_security_group_rule" "inbound_http_to_vm" {
+  security_group_id = aws_security_group.vm.id
+  description       = "Allow http protocol inbound from any IP"
+  type              = "ingress"
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "inbound_https_to_vm" {
+  security_group_id = aws_security_group.vm.id
+  description       = "Allow https protocol inbound from any IP"
+  type              = "ingress"
+  from_port         = "443"
+  to_port           = "443"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "inbound_ssh_to_vm" {
+  security_group_id = aws_security_group.vm.id
+  description       = "Allow ssh protocol inbound from any IP"
+  type              = "ingress"
+  from_port         = "22"
+  to_port           = "22"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "vm_outbound_any" {
+  security_group_id = aws_security_group.vm.id
+  description       = "Allow VM to connect out to any IP on any port"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.amazon_linux_23.id
   instance_type = "t2.micro"
