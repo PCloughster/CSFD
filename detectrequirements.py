d0ad4584230968cc429e7197a8651629552d8725
Patrick Clough <patrick@sibbit.co.uk>
2024-04-10 21:42:15 +0100

    detectrequirements.py clones, then deletes repository

--- detectrequirements.py
+++ detectrequirements.py
@@ -1,1 +1,20 @@
-# TODO - Create the script which will browse through the git repository to detect framework, required packages etc
+import os, shutil, time
+
+repo_name = "CSFD-test-repo"
+git_repo = "https://github.com/PCloughster/CSFD-test-repo"
+
+try:
+    shutil.rmtree(repo_name)
+    print("Directory already exists, removing") 
+except:
+    print("Directory doesn't exist, proceeding") 
+
+os.mkdir(repo_name)
+os.chdir(repo_name)
+os.system("touch test")
+os.system("git init")
+os.system("git clone "+git_repo)
+
+
+os.chdir("..")
+shutil.rmtree(repo_name)
